5e00fba2922bfc0d67bc7943e2fa0b0ca69ecff2
nyddogghr <nyddogghr@gmail.com>
2018-01-19 19:17:59 +0100

    Update admin urole in front

--- sixquiprend/static/js/main_controller.js
+++ sixquiprend/static/js/main_controller.js
@@ -6,7 +6,7 @@ app.controller('MainController', ['$rootScope', '$scope', '$http', 'growl',
     // UI
 
     $scope.is_admin = function() {
-      return $rootScope.current_user && $rootScope.current_user.urole == 3;
+      return $rootScope.current_user && $rootScope.current_user.urole == 2;
     };
 
   }
